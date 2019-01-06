63da880b28f68b822f5039ff1201417611b36fd3
KEEN <kyh0927@live.co.kr>
2019-01-06 12:39:16 +0900

    2019 01 06

--- src/App.js
+++ src/App.js
@@ -7,32 +7,11 @@ class App extends Component {
   }
 
   componentDidMount() {
-    setTimeout(() => {
-      this.setState({
-        movies: [
-          {
-            title: 'Matrix',
-            poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWdonEnEOvFIqxc0ORv_SmB5usUvGzjiZNdntX_HBFd9XOqIse9g'
-          },
-          {
-            title: 'starwars',
-            poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTJx9yyP5aRJ6nwAiP_mlmP5glaitpGtjyAs4r3GqrGj2Ftq0qW'
-          },
-          {
-            title: 'avatar',
-            poster: 'http://img.hani.co.kr/imgdb/resize/2010/0107/6000383621_20100107.JPG'
-          },
-          {
-            title: 'old boy',
-            poster: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3MTsEmFMOu8XXEl-0Dxc1bM3lQ1Ibtgk7_tax9zokTvKj6AiNrQ'
-          }, 
-          {
-            title: "bumblebee",
-            poster: 'http://www.visualdive.com/wp-content/uploads/2017/03/bumble.jpg'
-          }     
-        ]   
-      })
-    },5000)
+    fetch('https://yts.am/api/v2/list_movies.json?quality=3D?sort_by=like count')
+    .then( response => response.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
+
   }
 
   _renderMovies = () => {
--- src/Movie.js
+++ src/Movie.js
@@ -2,19 +2,6 @@ import React from 'react';
 import './Movie.css';
 import { PropTypes } from 'prop-types';
 
-// class Movie extends Component {
-
-//   render() {
-    
-//     return (
-//       <div>
-//         <MoviePoster poster={this.props.poster} />
-//         <h1>{this.props.title}</h1>
-//       </div>
-//     );
-//   }
-// }
-
 const Movie = ({title, poster}) => {
  return (
     <div>
@@ -30,9 +17,6 @@ const MoviePoster = ({poster}) => {
   )
 }
 
-// MoviePoster.propTypes = {
-//   poster: proptypes.string
-// }
 Movie.propTypes = {
   title: PropTypes.string,
   poster: PropTypes.string,
